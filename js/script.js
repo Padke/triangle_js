5e42f8fd327c84535791d077169d1d00bc6cbc16
Patrycja Kania <el.czupakabra21@gmail.com>
2018-03-08 13:43:53 +0100

    Add content to index.html and script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+var a = prompt('Enter triangle base: ');
+var h = prompt('Enter triangle height');
+var triangleArea = a * h / 2;
+
+document.write('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
